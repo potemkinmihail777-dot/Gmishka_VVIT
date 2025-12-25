d9e95382a0199f6c04b39deead95eb37214414a8
Gmishka <potemkinmihail777@gmail.com>
2025-12-25 17:52:44 +0300

    Update main.py

---
+++ lab4/task3/main.py
@@ -0,0 +1,13 @@
+import math 
+
+def summ(a, b): 
+    return a + b 
+
+
+def sq(n): 
+    if n < 0: 
+        return "Ошибка, число отрицательное" 
+    return math.sqrt(n)
+
+def stepen(a, b): 
+    return a ** b
